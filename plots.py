8e4965a8c052206350382de91e533a4a8f29dcc5
grg7221 <kopchenoe.salo@inbox.ru>
2026-01-17 12:34:08 +0300

    Add training metrics logging and plotting
    Enhanced train.py to log training and validation metrics (loss, accuracy, precision, recall, F1) to CSV for each epoch and save model checkpoints. Added new plotting utilities in plots.py to generate and save plots from these metrics.

--- plots.py
+++ plots.py
@@ -1,26 +1,63 @@
 import matplotlib.pyplot as plt
 from datasets import load_dataset
 from dataset.custom_tk import Tokenizer
+from pandas import read_csv
+import plotly.express as px
 
-tk = Tokenizer(500)
-def tokenize(batch):
-    return {
-        "tokens": [tk.encode(x, False) for x in batch['text']]
-    }
+def dataset_text_length():
+    tk = Tokenizer(500)
+    def tokenize(batch):
+        return {
+            "tokens": [tk.encode(x, False) for x in batch['text']]
+        }
 
-ds_train = load_dataset("stanfordnlp/imdb")['train']
-ds_train_tok = ds_train.map(tokenize, batched=True)
+    ds_train = load_dataset("stanfordnlp/imdb")['train']
+    ds_train_tok = ds_train.map(tokenize, batched=True)
 
-text_len = []
-for tokens in ds_train_tok['tokens']:
-    text_len.append(len(tokens))
+    text_len = []
+    for tokens in ds_train_tok['tokens']:
+        text_len.append(len(tokens))
 
-plt.figure(figsize=(10,6))
-plt.hist(text_len, 100)
+    plt.figure(figsize=(10,6))
+    plt.hist(text_len, 100)
 
-plt.xlabel("Length")
-plt.ylabel("Freq")
+    plt.xlabel("Length")
+    plt.ylabel("Freq")
 
-plt.savefig(f'plots/text_lengths.png', dpi=150)
+    plt.savefig(f'plots/text_lengths.png', dpi=150)
 
-plt.show()
+    plt.show()
+
+def train_plots(batch_size, embed_dim, num_filters):
+    df = read_csv(f'metrics/b{batch_size}_d{embed_dim}_f{num_filters}.csv')
+
+    def line_plot(df, y_cols, title, y_label, file_name):
+        d = df.melt(id_vars="epoch", value_vars=y_cols, var_name="metric", value_name="value")
+        fig = px.line(d, x="epoch", y="value", color="metric", markers=True, title=title, labels={"epoch": "Epoch", "value": y_label, "metric": ""})
+        fig.update_layout(template="plotly_white")
+        fig.write_image(f'plots/{file_name}_b{batch_size}_d{embed_dim}_f{num_filters}.png', width=900, height=500, scale=2)
+        #fig.show()
+
+    line_plot(
+        df,
+        y_cols=["train_loss", "val_loss"],
+        title="Loss: train vs validation",
+        y_label="Cross-entropy loss",
+        file_name='Loss'
+    )
+
+    line_plot(
+        df,
+        y_cols=["accuracy", "F1"],
+        title="Accuracy and F1",
+        y_label="Score",
+        file_name='Acc+F1'
+    )
+
+    line_plot(
+        df,
+        y_cols=["precision", "recall"],
+        title="Precision and Recall",
+        y_label="Score",
+        file_name='Prec+Rec'
+    )
--- train.py
+++ train.py
@@ -1,8 +1,9 @@
 from CNN import CNN
 from torch.optim import Adam
+from plots import train_plots
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
+import csv
 #from tqdm import trange
 
 torch.manual_seed(2506)
@@ -10,29 +11,37 @@ torch.manual_seed(2506)
 batch_size = 512
 vocab_size = 25000
 embed_dim = 128
-num_filters = 100
+num_filters = 200
 kernel_sizes = [3, 4, 5]
 epochs = 10
 val_split = 0.1
 lr = 1e-3 * (batch_size / 512)
 
+# создаем csv для текущего набора гиперпараметров
+with open(f'metrics/b{batch_size}_d{embed_dim}_f{num_filters}.csv', mode='w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['epoch', 'train_loss', 'val_loss', 'accuracy', 'precision', 'recall', 'F1'])
+
 data = torch.load('dataset/IMDB/train/train.pt', map_location='cuda')
 X = data[0].long()
 Y = data[1].long()
-N = len(X)
+N = len(X) # количество документов
 
+# шафлим
 perm = torch.randperm(N, device='cuda')
 X = X[perm]
 Y = Y[perm]
 
+# разделяем на train и val
 split = int(N*val_split)
-N -= split
+N -= split # количество док-ов в train split
 X_train = X[split:]
 Y_train = Y[split:]
 
 X_val = X[:split]
 Y_val = Y[:split]
 
+# инициализируем модель и оптимизатор
 model = CNN(
     vocab_size=vocab_size,
     embed_dim=embed_dim,          # C
@@ -40,14 +49,13 @@ model = CNN(
     kernel_sizes=kernel_sizes,
     num_filters=num_filters
 ).cuda().train()
-
 optimizer = Adam(model.parameters(), lr)
 
 print(f"Total params: {sum(p.numel() for p in model.parameters())}")
 
 for e in range(epochs):
+    # шафлим каждую эпоху
     perm = torch.randperm(N, device='cuda')
-
     X_shuffle = X_train[perm]
     Y_shuffle = Y_train[perm]
 
@@ -67,9 +75,8 @@ for e in range(epochs):
 
         total_loss += loss.item()
 
-    TP = FP = FN = TN = 0
-
     # val split
+    TP = FP = FN = TN = 0
     for i in range(0, len(X_val), batch_size):
         logits = model(X_val[i:i+batch_size])
         val_loss = F.cross_entropy(logits, Y_val[i:i+batch_size])
@@ -81,8 +88,21 @@ for e in range(epochs):
         FN += ((preds == 0) & (y == 1)).sum().item()
         TN += ((preds == 0) & (y == 0)).sum().item()
 
-    accuracy = (TP + TN) / (TP + TN + FP + FN)
-    precision = TP / (TP + FP + 1e-8)
-    recall = TP / (TP + FN + 1e-8)
+    # метрики
+    accuracy = (TP + TN) / (TP + TN + FP + FN) # доля правильных ответов
+    precision = TP / (TP + FP + 1e-8) # сколько реально positive из найденных
+    recall = TP / (TP + FN + 1e-8) # сколько positive найдено из всех
+    F1 = 2*(precision*recall)/(precision+recall)
     print(f"Epoch {e+1}: total_loss={total_loss:.4f}, last loss={loss.item():.4f}")
-    print(f"Val split: val_loss={val_loss:.4f} acc={accuracy:.4f}, precision={precision:.4f}, recall={recall:.4f}")
+    print(f"Val split: val_loss={val_loss.item():.4f} acc={accuracy:.4f}, precision={precision:.4f}, recall={recall:.4f}, F1={F1:.4f}")
+
+    with open(f'metrics/b{batch_size}_d{embed_dim}_f{num_filters}.csv', mode='a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([e, loss.item(), val_loss.item(), accuracy, precision, recall, F1])
+
+torch.save({
+    'model': model.state_dict(),
+    'optimizer': optimizer.state_dict(),
+}, f"checkpoints/b{batch_size}_d{embed_dim}_f{num_filters}.pt")
+
+train_plots(batch_size, embed_dim, num_filters)
